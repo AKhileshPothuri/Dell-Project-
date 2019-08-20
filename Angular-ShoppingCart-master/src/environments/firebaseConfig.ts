0a05e7dfbd1bbbb3624006c1d7cc1af7a9a4d238
Akhilesh Pothuri <pothuriakhilesh@yahoo.com>
2019-08-21 01:35:33 +0530

    Basic UI working

---
+++ Angular-ShoppingCart-master/src/environments/firebaseConfig.ts
@@ -0,0 +1,9 @@
+export const FireBaseConfig = {
+  apiKey: "AIzaSyAKd_yle9Q0Ieq4SMQ8Cs4GmBvqxbjBceA",
+  authDomain: "ecomm-trial-p1.firebaseapp.com",
+  databaseURL: "https://ecomm-trial-p1.firebaseio.com",
+  projectId: "ecomm-trial-p1",
+  storageBucket: "",
+  messagingSenderId: "261752892614",
+  appId: "1:261752892614:web:efa86300547cc76e"
+};
