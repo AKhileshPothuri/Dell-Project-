8bab123badfa01690200b3e2552bfbeb48ba2575
Raghuram Veeramallu <rockstar@dynamo.local>
2019-08-25 11:49:10 +0530

    small billings changes

--- Angular-ShoppingCart-master/e2e/protractor.conf.js
+++ Angular-ShoppingCart-master/e2e/protractor.conf.js
@@ -10,7 +10,7 @@ exports.config = {
     browserName: "chrome"
   },
   directConnect: true,
-  baseUrl: "http://localhost:4200/",
+  baseUrl: "http://ecomm-trial-p1.firebaseapp.com/",
   framework: "jasmine",
   jasmineNodeOpts: {
     showColors: true,
